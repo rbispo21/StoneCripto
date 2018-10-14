3d329191dd920c417ad28239e195fe00f2bc921e
Ruan Bispo <rbispo21@gmail.com>
2018-10-14 01:35:33 -0300

    add called for register

--- app/src/main/java/br/com/stone/stonecripto/welcome/WelcomeActivity.kt
+++ app/src/main/java/br/com/stone/stonecripto/welcome/WelcomeActivity.kt
@@ -1,13 +1,38 @@
 package br.com.stone.stonecripto.welcome
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import br.com.stone.stonecripto.R
+import br.com.stone.stonecripto.di.component.DaggerWelcomeComponent
+import br.com.stone.stonecripto.di.module.WelcomeModule
+import kotlinx.android.synthetic.main.activity_welcome.*
+import javax.inject.Inject
 
-class WelcomeActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity(), WelcomeContract.View {
+    @Inject
+    lateinit var presenter: WelcomeContract.Presenter
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val component = DaggerWelcomeComponent.builder().welcomeModule(WelcomeModule(this)).build()
+        component.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        configureClickButton()
+    }
+
+    private fun configureClickButton() {
+        btnContinue.setOnClickListener {
+            presenter.clickContinue(this)
+        }
+    }
+
+    override fun closeActivity() {
+        finish()
+    }
+
+    override fun callActivity(intent: Intent) {
+        startActivity(intent)
     }
 }
