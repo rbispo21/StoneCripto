645b40f488d509c55e9324cf82f78ebd7b2cdd37
Ruan Bispo <rbispo21@gmail.com>
2018-10-15 20:35:53 -0300

    add functions for control balance

--- app/src/main/java/br/com/stone/stonecripto/manager/CoinManager.kt
+++ app/src/main/java/br/com/stone/stonecripto/manager/CoinManager.kt
@@ -23,4 +23,72 @@ class CoinManager {
         }
         return 0.0
     }
+
+    fun updatePrice(type: CoinType, price: Double): Boolean {
+        val realm = Realm.getDefaultInstance()
+        val coin = realm.where(Coin::class.java).equalTo("name", type.name).findFirst()
+        coin?.let {itCoint ->
+            realm.executeTransaction {
+                itCoint.price = price
+            }
+            return true
+        }
+
+        realm.executeTransaction {
+            val coinRealm = realm.createObject(Coin::class.java)
+            coinRealm.name = type.name
+            coinRealm.balance = 0.00
+            coinRealm.price = price
+        }
+        return true
+    }
+
+    fun buyCoin(type: CoinType, amount: Double): Boolean {
+        if (hasBalance(type, amount)) {
+            payment(type, amount)
+            addCoin(type, amount)
+            return true
+        }else {
+            return false
+        }
+    }
+
+    private fun hasBalance(type: CoinType, amount: Double): Boolean {
+        val priceTotal = getPriceTotal(type, amount)
+        return if (priceTotal > 0) getBalance(CoinType.BRL) >= priceTotal else false
+    }
+
+    private fun payment(type: CoinType, amount: Double) {
+        val priceTotal = getPriceTotal(type, amount)
+        val realm = Realm.getDefaultInstance()
+        val coinBRL = realm.where(Coin::class.java).equalTo("name", CoinType.BRL.name).findFirst()
+        coinBRL?.let {itCoinBRL ->
+            itCoinBRL.balance?.let {itBalance ->
+                realm.executeTransaction {
+                    itCoinBRL.balance = itBalance - priceTotal
+                }
+            }
+        }
+    }
+
+    private fun addCoin(type: CoinType, amount: Double) {
+        val realm = Realm.getDefaultInstance()
+        val coin = realm.where(Coin::class.java).equalTo("name", type.name).findFirst()
+        coin?.let {itCoin ->
+            itCoin.balance?.let {itBalance ->
+                realm.executeTransaction {
+                    itCoin.balance = itBalance + amount
+                }
+            }
+        }
+    }
+
+    private fun getPriceTotal(type: CoinType, amount: Double): Double{
+        val realm = Realm.getDefaultInstance()
+        val coin = realm.where(Coin::class.java).equalTo("name", type.name).findFirst()
+        coin?.price?.let {
+            return amount * it
+        }
+        return 0.0
+    }
 }
--- app/src/main/java/br/com/stone/stonecripto/model/Coin.kt
+++ app/src/main/java/br/com/stone/stonecripto/model/Coin.kt
@@ -2,4 +2,4 @@ package br.com.stone.stonecripto.model
 
 import io.realm.RealmObject
 
-open class Coin(var name: String? = null, var balance: Double? = 0.0): RealmObject()
+open class Coin(var name: String? = null, var balance: Double? = 0.0, var price: Double? = 0.0): RealmObject()
